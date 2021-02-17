b7cf4340603c78537bb1b1db4177cfc57e21823e
howard chen <chenhoward0401@gmail.com>
2021-02-17 14:18:39 +0800

    feat: [Howard] 1. test keepalive

--- cmd/client/client.go
+++ cmd/client/client.go
@@ -11,28 +11,43 @@ import (
 
 func main() {
 	ctx := context.Background()
-	// Set Client Deadline
-	clientDeadline := time.Now().Add(time.Duration(100 * time.Second))
-	ctx, cancel := context.WithDeadline(ctx, clientDeadline)
-	defer cancel()
+
 	// Set Connection
-	conn, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
+	opts := []grpc.DialOption{
+		grpc.WithInsecure(),
+	}
+	conn, err := grpc.DialContext(ctx, "localhost:50051", opts...)
 	if err != nil {
 		log.Fatalf("Conn Err: %v", err)
 	}
 	defer conn.Close()
 
-	// Call TestGreet function by grpc
+	// Connection
 	c := pb.NewHelloClient(conn)
+
+	// Set Client Deadline
+	// userCostTime := 90
+	// serverCostTime := 30
+	clientDeadline := time.Now().Add(time.Duration((90 + 100) * time.Second))
+	ctx, cancel := context.WithDeadline(ctx, clientDeadline)
+	defer cancel()
+
+	// Wait for auth
+	// time.Sleep(20 * time.Second)
+
+	// Call TestGreet function check whether auth is valid
 	res, err := c.TestGreet(ctx, &pb.GreetRequest{Say: "golang grpc000"})
-	select {
-	case <-ctx.Done():
-		log.Panicf("err: %v", ctx.Err())
-	default:
-		log.Println("ctx works well")
-	}
 	if err != nil {
 		log.Fatalf("c.TestGreet Error:%v", err)
+	} else {
+		log.Printf("res = %v", res)
 	}
-	log.Printf("res = %v", res)
+
+	select {
+	case <-ctx.Done():
+		log.Printf("client side err: %v", ctx.Err())
+	default:
+		log.Println("client side ctx works well")
+	}
+
 }
--- cmd/server/server.go
+++ cmd/server/server.go
@@ -10,6 +10,8 @@ import (
 	"time"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/keepalive"
+	"google.golang.org/grpc/reflection"
 )
 
 type Server struct {
@@ -17,14 +19,34 @@ type Server struct {
 
 func main() {
 	fmt.Println("hi, welcome to server..")
-	ctx := context.Background()
-	listen, _ := net.Listen("tcp", "0.0.0.0:50051")
 
+	var kaep = keepalive.EnforcementPolicy{
+		MinTime:             3 * time.Second, // If a client pings more than once every 5 seconds, terminate the connection
+		PermitWithoutStream: true,            // Allow pings even when there are no active streams
+	}
+	var kasp = keepalive.ServerParameters{
+		// MaxConnectionIdle:     2 * time.Second, // If a client is idle for 15 seconds, send a GOAWAY
+		MaxConnectionAge:      10 * time.Second, // If any connection is alive for more than 90 seconds, send a GOAWAY
+		MaxConnectionAgeGrace: 1 * time.Second,  // Allow 20 seconds for pending RPCs to complete before forcibly closing connections
+		Time:                  5 * time.Second,  // Ping the client if it is idle for 5 seconds to ensure the connection is still active
+		Timeout:               3 * time.Second,  // Wait 3 second for the ping ack before assuming the connection is dead
+	}
+
+	ctx := context.Background()
+	listen, err := net.Listen("tcp", "0.0.0.0:50051")
+	if err != nil {
+		fmt.Printf("err when listen tcp: %v", err)
+	}
+	// Connection Timeout Setting
 	// opts := []grpc.ServerOption{grpc.ConnectionTimeout(120 * time.Second)}
 	// grpcServer := grpc.NewServer(opts...)
-	grpcServer := grpc.NewServer()
+	// // grpcServer := grpc.NewServer()
 
 	s := &Server{}
+
+	grpcServer := grpc.NewServer(grpc.KeepaliveEnforcementPolicy(kaep), grpc.KeepaliveParams(kasp))
+	reflection.Register(grpcServer)
+	// pb.RegisterHelloServer(s, &servers)
 	pb.RegisterHelloServer(grpcServer, s)
 	c := make(chan os.Signal, 1)
 
@@ -35,21 +57,40 @@ func main() {
 		}
 	}()
 
-	grpcServer.Serve(listen)
+	err = grpcServer.Serve(listen)
+	if err != nil {
+		fmt.Printf("err when serve:%v", err)
+	}
 }
 
 // TestGreet function for grpc
 func (s *Server) TestGreet(ctx context.Context, req *pb.GreetRequest) (*pb.GreetResponse, error) {
+
+	// set backend auth timeout
+	ctx, cancel := context.WithTimeout(ctx, 100*time.Second)
+	defer cancel()
+
+	// Assume auth cost 20 sec
 	fmt.Printf("begining time %v\n", time.Now())
-	time.Sleep(90 * time.Second)
+	time.Sleep(10 * time.Second)
 	fmt.Printf("receiving time %v\n", time.Now())
-	select {
-	default:
-	case <-ctx.Done():
-		log.Panicf("err: %v", ctx.Err())
+
+	if ctx.Err() == context.Canceled {
 		return nil, ctx.Err()
 	}
+
+	// If ctx got something wrong
+	select {
+	case <-ctx.Done():
+		log.Printf("TestGreet err: %v", ctx.Err())
+		return nil, ctx.Err()
+	default:
+		log.Println("server side: TestGreet ctx works well")
+	}
+
+	// retrun message
 	req.Say = fmt.Sprintf("Received: %v", req.Say)
 	log.Printf("Receive:%v", req.Say)
 	return &pb.GreetResponse{Receive: req.Say}, nil
+
 }
